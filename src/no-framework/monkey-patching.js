d3d1bc13595048579a2a88b95e1f158822854e71
Hector Coronado <hector.coronado@autodesk.com>
2020-06-24 14:47:42 -0700

    illustrate monkey-patching process by example

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -2,11 +2,19 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+/**
+ * "Monkey-patching" illustrated by the following process:
+ */
+
+// create copy of the utility function as it exists
 const originalGetWinner = utils.getWinner
+
+// override it with deterministic function that tests
+// as we expect
 utils.getWinner = (p1, p2) => p1
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const winner = thumbWar('Kent C Dodds', 'Ken Wheeler')
+assert.strictEqual(winner, 'Kent C Dodds')
 
-// cleanup
+// re-assign the utility to original
 utils.getWinner = originalGetWinner
