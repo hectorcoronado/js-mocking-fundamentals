889cf8975ada97e21d112f90af62be2889b47b41
Hector Coronado <hector.coronado@autodesk.com>
2020-06-24 16:31:27 -0700

    spoof jest fn function

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -1,29 +1,31 @@
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+
 test('returns winner', () => {
   const originalGetWinner = utils.getWinner
   utils.getWinner = jest.fn((p1, p2) => p1)
 
-  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
-  expect(utils.getWinner.mock.calls).toEqual([
-    ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler']
-  ])
-  // could also do these assertions:
+  const winner = thumbWar('Kent C Dodds', 'Ken Wheeler')
+  expect(winner).toBe('Kent C Dodds')
   expect(utils.getWinner).toHaveBeenCalledTimes(2)
+  expect(utils.getWinner).toHaveBeenCalledWith('Kent C Dodds', 'Ken Wheeler')
   expect(utils.getWinner).toHaveBeenNthCalledWith(
     1,
-    'Kent C. Dodds',
+    'Kent C Dodds',
     'Ken Wheeler'
   )
   expect(utils.getWinner).toHaveBeenNthCalledWith(
     2,
-    'Kent C. Dodds',
+    'Kent C Dodds',
     'Ken Wheeler'
   )
+  // console.log('utils.getWinner:', utils.getWinner.mock)
+  expect(utils.getWinner.mock.calls).toEqual([
+    [ 'Kent C Dodds', 'Ken Wheeler' ],
+    [ 'Kent C Dodds', 'Ken Wheeler' ]
+  ])
 
-  // cleanup
+  // cleanup:
   utils.getWinner = originalGetWinner
 })
--- src/no-framework/mock-fn.js
+++ src/no-framework/mock-fn.js
@@ -2,13 +2,24 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
-function fn(impl = () => {}) {
+/**
+ * jest's *fn* function accepts a callback as an argument,
+ * called 'implementation' ('impl' for short), and it
+ *  - returns that impl func, called with all the arguments,
+ *  - keeps track of which arguments have been passed to impl
+ * @param {function} impl 
+ */
+const fn = impl => {
   const mockFn = (...args) => {
+    console.log('...args:', ...args)
+    // keep track of arguments passed in to impl:
     mockFn.mock.calls.push(args)
+    // call impl with arguments
     return impl(...args)
   }
+
   mockFn.mock = {calls: []}
-  mockFn.mockImplementation = newImpl => (impl = newImpl)
+
   return mockFn
 }
 
