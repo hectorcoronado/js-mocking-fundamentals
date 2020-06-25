a382624d6f65ed5fb3f22383157cbfeba82bfe81
Hector Coronado <hector.coronado@autodesk.com>
2020-06-25 09:20:21 -0700

    revert __tests__/inline-module-mock.js to original state

--- src/__tests__/inline-module-mock.js
+++ src/__tests__/inline-module-mock.js
@@ -1,12 +1,6 @@
 const thumbWar = require('../thumb-war')
 const utilsMock = require('../utils')
 
-jest.mock('../utils', () => {
-  return {
-    getWinner: jest.fn((p1, p2) => p1)
-  }
-})
-
 test('returns winner', () => {
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
   expect(winner).toBe('Kent C. Dodds')
