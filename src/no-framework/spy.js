312a939749e205d60159e9ac70354476cd062b91
Hector Coronado <hector.coronado@autodesk.com>
2020-07-02 08:18:37 -0700

    comment on how spyOn function works

--- src/no-framework/spy.js
+++ src/no-framework/spy.js
@@ -2,6 +2,10 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+/**
+ * 
+ * @param {function} impl default implementation
+ */
 function fn(impl = () => {}) {
   const mockFn = (...args) => {
     mockFn.mock.calls.push(args)
@@ -14,11 +18,19 @@ function fn(impl = () => {}) {
 
 // const originalGetWinner = utils.getWinner
 // utils.getWinner = fn((p1, p2) => p2)
+
+/**
+ * 
+ * @param {object} obj with value that we need to monkeypatch
+ * @param {string} obj's property name that we need to monkeypatch 
+ */
 const spyOn = (obj, prop) => {
   // track the original value that we need to override/monkey-patch
   const originalValue = obj[prop]
   
+  // monkeypatch that value
   obj[prop] = fn()
+
   // restore the original value:
   obj[prop].mockRestore = () => (obj[prop] = originalValue)
 }
