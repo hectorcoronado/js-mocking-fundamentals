52998d70fc021b56454bfc7305ec78a9d7618783
Hector Coronado <hector.coronado@autodesk.com>
2020-09-10 15:13:06 -0700

    use spyOn to mock js func & restore to original val

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -1,31 +1,44 @@
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
+test("returns winner", () => {
+  const originalGetWinner = utils.getWinner;
+  // jest's fn can be provided an implementation
+  // of a function that we're mocking; it can keep
+  // track of the arguments it's called with (these
+  // can be seen under the `mock` property of whatever
+  // )
+  utils.getWinner = jest.fn((p1, p2) => p1);
 
-test('returns winner', () => {
-  const originalGetWinner = utils.getWinner
-  utils.getWinner = jest.fn((p1, p2) => p1)
+  const winner = thumbWar("Kent C Dodds", "Ken Wheeler");
+  console.log(utils.getWinner.mock);
 
-  const winner = thumbWar('Kent C Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C Dodds')
-  expect(utils.getWinner).toHaveBeenCalledTimes(2)
-  expect(utils.getWinner).toHaveBeenCalledWith('Kent C Dodds', 'Ken Wheeler')
+  expect(winner).toBe("Kent C Dodds");
+
+  /*
+  expect(utils.getWinner).toHaveBeenCalledTimes(2);
+  expect(utils.getWinner).toHaveBeenCalledWith("Kent C Dodds", "Ken Wheeler");
   expect(utils.getWinner).toHaveBeenNthCalledWith(
     1,
-    'Kent C Dodds',
-    'Ken Wheeler'
-  )
+    "Kent C Dodds",
+    "Ken Wheeler"
+  );
   expect(utils.getWinner).toHaveBeenNthCalledWith(
     2,
-    'Kent C Dodds',
-    'Ken Wheeler'
-  )
-  // console.log('utils.getWinner:', utils.getWinner.mock)
+    "Kent C Dodds",
+    "Ken Wheeler"
+  );
+  */
+
+  // all the above commented asssertions can be covered with
+  // `mock.calls`, because it's ascertaining the no. of times
+  // the function is called, which args it's passed and in which
+  // order:
   expect(utils.getWinner.mock.calls).toEqual([
-    [ 'Kent C Dodds', 'Ken Wheeler' ],
-    [ 'Kent C Dodds', 'Ken Wheeler' ]
-  ])
+    ["Kent C Dodds", "Ken Wheeler"],
+    ["Kent C Dodds", "Ken Wheeler"],
+  ]);
 
   // cleanup:
-  utils.getWinner = originalGetWinner
-})
+  utils.getWinner = originalGetWinner;
+});
--- src/no-framework/spy.js
+++ src/no-framework/spy.js
@@ -1,50 +1,50 @@
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
 /**
- * 
+ *
  * @param {function} impl default implementation
  */
 function fn(impl = () => {}) {
   const mockFn = (...args) => {
-    mockFn.mock.calls.push(args)
-    return impl(...args)
-  }
-  mockFn.mock = {calls: []}
-  mockFn.mockImplementation = newImpl => (impl = newImpl)
-  return mockFn
+    mockFn.mock.calls.push(args);
+    return impl(...args);
+  };
+  mockFn.mock = { calls: [] };
+  mockFn.mockImplementation = (newImpl) => (impl = newImpl);
+  return mockFn;
 }
 
 // const originalGetWinner = utils.getWinner
 // utils.getWinner = fn((p1, p2) => p2)
 
 /**
- * 
+ *
  * @param {object} obj with value that we need to monkeypatch
- * @param {string} obj's property name that we need to monkeypatch 
+ * @param {string} prop obj's property name that we need to monkeypatch
  */
 const spyOn = (obj, prop) => {
   // track the original value that we need to override/monkey-patch
-  const originalValue = obj[prop]
-  
+  const originalValue = obj[prop];
+
   // monkeypatch that value
-  obj[prop] = fn()
+  obj[prop] = fn();
 
   // restore the original value:
-  obj[prop].mockRestore = () => (obj[prop] = originalValue)
-}
+  obj[prop].mockRestore = () => (obj[prop] = originalValue);
+};
 
-spyOn(utils, 'getWinner')
-utils.getWinner.mockImplementation((p1, p2) => p1)
+spyOn(utils, "getWinner");
+utils.getWinner.mockImplementation((p1, p2) => p1);
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+assert.strictEqual(winner, "Kent C. Dodds");
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
-  ['Kent C. Dodds', 'Ken Wheeler'],
-  ['Kent C. Dodds', 'Ken Wheeler']
-])
+  ["Kent C. Dodds", "Ken Wheeler"],
+  ["Kent C. Dodds", "Ken Wheeler"],
+]);
 
 // cleanup
 // utils.getWinner = originalGetWinner
-utils.getWinner.mockRestore()
+utils.getWinner.mockRestore();
